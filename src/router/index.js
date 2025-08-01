76839561ff3b1ff84de9a5160ce0b61b1438a51e
patricklinzzz <p9011910pat@gmail.com>
2025-08-02 01:18:35 +0800

    fix-調整打包的設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
-import HomeView from '@/views/HomeView.vue'
-import navbar from '@/components/navbar.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: navbar,
+      component: () => import('@/components/navbar.vue'),
       meta: { requiresAuth: true },
       children: [
         {
           path: '',
           name: 'home',
-          component: HomeView,
+          component: () => import('@/views/HomeView.vue'),
         },
         {
           path: '/new',

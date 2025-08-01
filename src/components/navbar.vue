2ac6b40b95a900cbba1b07ad8411db057521315c
patricklinzzz <p9011910pat@gmail.com>
2025-08-02 02:38:53 +0800

    fix-調整排版 以及導覽列邏輯架構

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,8 +1,35 @@
 <script setup>
   import { useAuthStore } from '@/stores/auth'
-  import { RouterLink } from 'vue-router'
+  import { useRoute } from 'vue-router'
   const authStore = useAuthStore()
   import logo from '@/assets/images/logo.svg'
+
+  const route = useRoute()
+
+  const menuItems = [
+    { index: '/連結寫這', title: '會員管理' },
+    { index: '/連結寫這', title: '活動管理' },
+    { index: '/new', title: '最新消息管理' },
+    {
+      id: 'edu', 
+      title: '教育測驗管理',
+      children: [
+        { index: '/連結寫這', title: '測驗類別管理' },
+        { index: '/連結寫這', title: '測驗題目管理' }
+      ]
+    },
+    {
+      id: 'shop', 
+      title: '商店管理',
+      children: [
+        { index: '/連結寫這', title: '商品管理' },
+        { index: '/連結寫這', title: '訂單管理' },
+        { index: '/連結寫這', title: '優惠券管理' }
+      ]
+    },
+    { index: '/連結寫這', title: '管理員設定' }
+  ]
+
   const handleOpen = (key, keyPath) => {
     // console.log(key, keyPath)
   }
@@ -12,112 +39,88 @@
 </script>
 
 <template>
-  <el-menu class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
-    <router-link to="/">
-      <div class="logo-container">
-        <img :src="logo" alt="" class="logo" />
-        <h2>後台管理</h2>
-      </div>
-    </router-link>
-    <el-menu-item index="1">
-      <router-link to="/連結放這">
-        <h3>會員管理</h3>
+  <el-container class="layout-container">
+    <el-aside>
+      <router-link to="/" class="logo-router-link">
+        <div class="logo-container">
+          <img :src="logo" alt="" class="logo" />
+          <h2>後台管理</h2>
+        </div>
       </router-link>
-    </el-menu-item>
+      <el-menu
+        :default-active="route.path"
+        router
+        @open="handleOpen"
+        @close="handleClose"
+      >
+        <template v-for="item in menuItems" :key="item.index || item.id">
+          <el-sub-menu v-if="item.children" :index="item.id">
+            <template #title>
+              <span>{{ item.title }}</span>
+            </template>
+            <el-menu-item v-for="child in item.children" :key="child.index" :index="child.index">
+              {{ child.title }}
+            </el-menu-item>
+          </el-sub-menu>
+          <el-menu-item v-else :index="item.index">
+            <span>{{ item.title }}</span>
+          </el-menu-item>
+        </template>
 
-    <el-menu-item index="2">
-      <router-link to="/">
-        <h3>活動管理</h3>
-      </router-link>
-    </el-menu-item>
-
-    <el-menu-item index="3">
-      <router-link to="/new">
-        <h3>最新消息管理</h3>
-      </router-link>
-    </el-menu-item>
-
-    <el-sub-menu index="4">
-      <template #title>
-        <router-link to="/">
-          <h3>教育測驗管理</h3>
-        </router-link>
-      </template>
-      <el-menu-item index="4-1">
-        <router-link to="/">
-          <h3>測驗類別管理</h3>
-        </router-link>
-      </el-menu-item>
-      <el-menu-item index="4-2">
-        <router-link to="/">
-          <h3>測驗題目管理</h3>
-        </router-link>
-      </el-menu-item>
-    </el-sub-menu>
-    <el-sub-menu index="5">
-      <template #title>
-        <router-link to="/">
-          <h3>商店管理</h3>
-        </router-link>
-      </template>
-      <el-menu-item index="5-1">
-        <router-link to="/">
-          <h3>商品管理</h3>
-        </router-link>
-      </el-menu-item>
-      <el-menu-item index="5-2">
-        <router-link to="/">
-          <h3>訂單管理</h3>
-        </router-link>
-      </el-menu-item>
-      <el-menu-item index="5-3">
-        <router-link to="/">
-          <h3>優惠券管理</h3>
-        </router-link>
-      </el-menu-item>
-    </el-sub-menu>
-    <el-menu-item index="6">
-      <router-link to="/">
-        <h3>管理員設定</h3>
-      </router-link>
-    </el-menu-item>
-
-    <el-menu-item index="7">
-      <h3 @click="authStore.logout()">登出</h3>
-    </el-menu-item>
-  </el-menu>
-  <router-view></router-view>
+        <el-menu-item @click="authStore.logout()">
+          <span>登出</span>
+        </el-menu-item>
+      </el-menu>
+    </el-aside>
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
 </template>
 
 <style scoped lang="scss">
-  h2,
-  h3 {
+  h2 {
     font-size: 1.125em;
+  }
+  .layout-container {
+    height: 100vh;
+  }
+  .el-aside {
+    width: fit-content;
+    background-color: #012038;
+    display: flex;
+    flex-direction: column;
+  }
+  .el-main {
+    overflow-y: auto;
   }
   .el-menu {
     --el-menu-active-color: #ec6c26;
-    --el-menu-bg-color: #012038;
+    --el-menu-bg-color: transparent;
     --el-menu-text-color: #fff;
     --el-menu-hover-bg-color: #001529;
     width: fit-content;
-    min-height: 100vh;
+    flex-grow: 1;
     border-right: none;
+    .el-menu-item,
+    :deep(.el-sub-menu__title) {
+      font-size: 1.125em;
+    }
     .el-button {
       float: right;
       margin-right: 20px;
       margin-top: 20px;
     }
-    a {
-      text-decoration: none;
-      color: inherit;
-    }
+  }
+  .logo-router-link {
+    text-decoration: none;
   }
   .logo-container {
     display: flex;
     flex-direction: column;
     align-items: center;
     color: #fff;
-    padding: 10px 0 10px;
+    padding: 20px 0;
     gap: 10px;
     .logo {
       height: 2em;

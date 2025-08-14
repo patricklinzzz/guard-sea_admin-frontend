75374fa6647c4ec46786746ed71557b29132cdde
patricklinzzz <p9011910pat@gmail.com>
2025-08-14 23:45:47 +0800

    feat-admin串資料庫、狀態編輯

--- src/stores/admin_store.js
+++ src/stores/admin_store.js
@@ -1,45 +1,19 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import axios from 'axios'
 
 export const useAdminStore = defineStore('admin', () => {
-  const admins = ref([
-    {
-      id: 'EMP001',
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      phone: '0912-345-678',
-    },
-    {
-      id: 'EMP002',
-      name: 'Jane Smith',
-      email: 'jane.smith@example.com',
-      phone: '0922-456-789',
-    },
-    {
-      id: 'EMP003',
-      name: 'Emily Davis',
-      email: 'emily.davis@example.com',
-      phone: '0933-567-890',
-    },
-    {
-      id: 'EMP004',
-      name: 'Michael Brown',
-      email: 'michael.brown@example.com',
-      phone: '0944-678-901',
-    },
-    {
-      id: 'EMP005',
-      name: 'Sarah Wilson',
-      email: 'sarah.wilson@example.com',
-      phone: '0955-789-012',
-    },
-    {
-      id: 'EMP006',
-      name: 'David Lee',
-      email: 'david.lee@example.com',
-      phone: '0966-890-123',
-    },
-  ])
+  const admins = ref([])
+
+  const fetchAdmins = async () => {
+    try {
+      const res = await axios.get('https://tibamef2e.com/cjd101/g1/api/admins/get_admins.php')
+      admins.value = res.data
+      console.log('Admin data fetched successfully:', admins.value)
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
   const addAdmin = (adminData) => {
     admins.value.unshift(adminData)
@@ -56,5 +30,26 @@ export const useAdminStore = defineStore('admin', () => {
     }
   }
 
-  return { admins, addAdmin, getAdminById, updateAdmin }
+  //管理員狀態
+  const updateAdminStatus = async (adminId, newStatus) => {
+    try {
+      const formData = new FormData()
+      formData.append('administrator_id', adminId) 
+      formData.append('status', newStatus)
+
+      const response = await axios.post(
+        'https://tibamef2e.com/cjd101/g1/api/admins/update_admin_status.php', 
+        formData
+      )
+      if (response.data.success) {
+        await fetchAdmins() 
+      } else {
+        console.error(response.data.error)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  return { admins, fetchAdmins, addAdmin, getAdminById, updateAdmin, updateAdminStatus }
 })
